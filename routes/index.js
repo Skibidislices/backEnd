db85e6e62d8b81198578730d61d5a2aee486d3ab
mvv7777 <michael.odabash@gmail.com>
2024-07-04 18:47:38 +0200

    Excel mapping

--- routes/index.js
+++ routes/index.js
@@ -22,19 +22,23 @@ router.post('/auth/reset-password', resetPasswordRequest);
 router.post('/auth/reset-password/:token', resetPassword);
 
 
-// File upload route
 router.post('/upload-excel', authMiddleware, upload.single('file'), (req, res) => {
   try {
     const file = req.file;
     const workbook = xlsx.read(file.buffer, { type: 'buffer' });
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
-    const json = xlsx.utils.sheet_to_json(worksheet);
-    res.json(json);
+    const jsonResult = {};
+
+    workbook.SheetNames.forEach(sheetName => {
+      const worksheet = workbook.Sheets[sheetName];
+      jsonResult[sheetName] = xlsx.utils.sheet_to_json(worksheet);
+    });
+
+    res.json(jsonResult);
   } catch (error) {
     console.error('Error processing file:', error);
     res.status(500).send('Error processing file');
   }
 });
+
 
 export default router;
