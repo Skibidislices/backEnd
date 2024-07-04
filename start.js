538835830a0ecb1b56d5aa185bf12c1f1cefcbf8
mvv7777 <michael.odabash@gmail.com>
2024-07-04 13:04:21 +0200

    Fix cors to allow access

--- start.js
+++ start.js
@@ -3,10 +3,12 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: 'variables.env' });
 import indexRouter from './routes/index.js';
+import cors from 'cors';
 
 const app = express();
 
 // support json encoded and url-encoded bodies, mainly used for post and update
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
